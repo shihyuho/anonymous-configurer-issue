345b95adb928d3146703bcd07d3eb543cb0148da
Shihyu Ho <methodho@gmail.com>
2024-04-22 14:24:06 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# anonymous-configurer-issue
+Sample code to report to Spring team
+
+- [AnonymousNotWorkTests](./src/test/java/com/example/demo/AnonymousNotWorkTests.java) - This contains a test case that fails.
+- [WorkaroundTests](./src/test/java/com/example/demo/WorkaroundTests.java) - This contains a workaround case.
+
+ref: [#14941](https://github.com/spring-projects/spring-security/issues/14941)
---
+++ src/test/java/com/example/demo/AnonymousNotWorkTests.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class AnonymousNotWorkTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnMyCustomAnonymousConfig() throws Exception {
+        this.mockMvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("myAnonymousUser"));
+    }
+
+
+    @EnableWebSecurity
+    @TestConfiguration
+    static class MvcMatcherServletPathConfig {
+
+
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            return http
+                    .with(new MyCustomDsl(), withDefaults())
+                    .build();
+        }
+
+        @RestController
+        static class PathController {
+
+            @RequestMapping("/api")
+            String api() {
+                return SecurityContextHolder.getContext().getAuthentication().getName();
+            }
+        }
+    }
+
+    /**
+     * https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#jc-custom-dsls
+     */
+    public static class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+
+        @Override
+        public void init(HttpSecurity http) throws Exception {
+            // any method that adds another configurer
+            // must be done in the init method
+            http.anonymous(anonymous -> anonymous.principal("myAnonymousUser"));
+        }
+    }
+}
